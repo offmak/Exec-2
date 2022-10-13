af471a583bc1fcfdc4c5e2eae82e11564cdb98e3
offmak <levyvitor606@gmail.com>
2022-10-13 09:09:32 -0300

    line7

--- js/main.js
+++ js/main.js
@@ -15,14 +15,4 @@ perguntasResposta.forEach(function (item){
 
 
 
-//  forma simples trabalhar com valores de uma array ou mais onde usa o mesmo lugar da 1 com da segunda 
 
-// const itensPerguntasERespostas = document.querySelectorAll(".item");
-
-// itensPerguntasERespostas.forEach(function(item) {
-//     item.addEventListener('click',function (){
-//         item.classList.add("ativo");
-//         const itemAtivoAtual = document.querySelector(".ativo");
-//         itemAtivoAtual.classList.remove("ativo")
-//     })
-// })
